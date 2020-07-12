709ddce38bcf108e9b8821ce17f6a4cfe2555711
Koddiak <lnevarez@protonmail.com>
2020-07-11 23:33:19 -0700

    Add code.
    Added code to the "BooleanLogic" and "Assignments" folders.

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Console.WriteLine(true && true && true || false);
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+
+            Console.ReadLine();
+        }
+    }
+}
