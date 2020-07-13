41892755ca12a408723aee34fca719cff8eab04c
Koddiak <lnevarez@protonmail.com>
2020-07-12 19:35:59 -0700

    Add code.
    Added code to the "StringFunctions" and "Assignments" folders.

---
+++ StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main()
+        {
+            //string name = "Jesse";
+            //string quote = "The name said, \"Hello\", Jesse.\nHello on a new line.\n\tHello on a tab.";
+            //string fileName = "C:\\Users\\Jesse";
+
+            //bool trueOrFalse = name.Contains("s");
+            //trueOrFalse = name.EndsWith("s");
+
+            //int length = name.Length;
+
+            //name = name.ToLower();
+
+            //Console.WriteLine(name);
+            //Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Jesse");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
