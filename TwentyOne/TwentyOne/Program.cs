deea8bd3108281e2719f326f68dc925b5f08e6fe
Koddiak <lnevarez@protonmail.com>
2020-07-16 23:16:51 -0700

    Add code.
    Added code to the "Assignments" folder.

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -11,8 +11,7 @@ namespace TwentyOne
         static void Main(string[] args)
         {
             Deck deck = new Deck();
-            int timesShuffled = 0;
-            deck = Shuffle(deck: deck, out timesShuffled, times: 3);
+            deck.Shuffle();
 
             foreach (Card card in deck.Cards)
             {
@@ -20,29 +19,7 @@ namespace TwentyOne
             }
 
             Console.WriteLine(deck.Cards.Count);
-            Console.WriteLine("Times shuffled: {0}", timesShuffled);
             Console.ReadLine();
-        }
-
-        public static Deck Shuffle(Deck deck, out int timesShuffled, int times = 1)
-        {
-            timesShuffled = 0;
-            for (int i = 0; i < times; i++)
-            {
-                timesShuffled++;
-                List<Card> tempList = new List<Card>();
-                Random random = new Random();
-
-                while (deck.Cards.Count > 0)
-                {
-                    int randomIndex = random.Next(deck.Cards.Count);
-                    tempList.Add(deck.Cards[randomIndex]);
-                    deck.Cards.RemoveAt(randomIndex);
-                }
-                deck.Cards = tempList;
-            }
-
-            return deck;
         }
 
         //public static Deck Shuffle(Deck deck, int times)
