0a1539ef07a5ebd2e271c4b09babb414440f9615
Koddiak <lnevarez@protonmail.com>
2020-07-18 23:20:11 -0700

    Add code.
    Added code to the "Assignments" folder and modified code in the "TwentyOne" folder.

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TwentyOne
 {
@@ -10,15 +11,35 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            Deck deck = new Deck();
-            deck.Shuffle(3);
+            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
+            string playerName = Console.ReadLine();
 
-            foreach (Card card in deck.Cards)
+            Console.WriteLine("And how much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
+
+            if (answer == "yes" || answer == "yeah" || answer == "yeh" || answer == "yup" || answer == "yah" || answer == "ya"  ||
+                answer == "y" || answer == "sure" || answer == "okay" || answer == "ok" || answer == "k" || answer == "kay"
+                || answer == "alright" || answer == "alrighty" || answer == "alrighty then" || answer == "aight")
             {
-                Console.WriteLine(card.Face + " of " + card.Suit);
+                Player player = new Player(playerName, bank);
+                Game game = new TwentyOneGame();
+                game += player;
+                player.isActivelyPlaying = true;
+                
+                while (player.isActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+
+                game -= player;
+
+                Console.WriteLine("Thank you for playing!");
             }
 
-            Console.WriteLine(deck.Cards.Count);
+            Console.WriteLine("Feel free to look around the casino. Bye for now!");
             Console.ReadLine();
         }
     }
