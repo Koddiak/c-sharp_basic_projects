e3f95e6c4f8b7ef34c9e29ea13b0a0bad9fdbe83
Koddiak <lnevarez@protonmail.com>
2020-07-19 20:30:28 -0700

    Add code.
    Added code to the "Assignments" folder and modified code in the "TwentyOne" folder.

---
+++ Assigments/assignment-page-234/assignment-page-234/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_page_234
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                int age = 0;
+                Console.WriteLine("What is your age?");
+                bool validAnswer = int.TryParse(Console.ReadLine(), out age);
+
+                Console.Write(Environment.NewLine);
+
+                if (!validAnswer)
+                {
+                    Console.WriteLine("Please enter an integer representation of your age in years.");
+                }
+                else if (age <= 0)
+                {
+                    Console.WriteLine("Please enter a valid age in years.");
+                }
+                else if (age == 1)
+                {
+                    Console.WriteLine("You are {0} year old.", age);
+                }
+                else
+                {
+                    Console.WriteLine("You are {0} years old.", age);
+                }
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error! " + ex.Message);
+            }
+        }
+    }
+}
