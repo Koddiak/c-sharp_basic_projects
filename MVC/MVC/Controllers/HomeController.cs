ca0aa750169186cb277087145c02a0f1fef0c3bb
Koddiak <lnevarez@protonmail.com>
2020-07-29 10:38:17 -0700

    Add code.
    Added code to the "Assignments" and "NewsletterAppMVC" folders.

--- MVC/MVC/Controllers/HomeController.cs
+++ MVC/MVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,19 +11,56 @@ namespace MVC.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            //--
+            //string text = "Hello";
+            //System.IO.File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\log.txt", text);
+
+            //--
+            //Random rnd = new Random();
+            //int num = rnd.Next();
+
+            //if (num > 20000)
+            //{
+            //    return View("About");
+            //}
+
+            //--
+            //return Content("Hello");
+
+            //--
+            //return RedirectToAction("Contact");
+
+            //--
+            //List<string> names = new List<string>
+            //{
+            //    "Jesse",
+            //    "Adam",
+            //    "Brett"
+            //};
+
+            //return View(names);
+
+            User user = new User();
+            user.Id = 1;
+            user.FirstName = "Bruce";
+            user.LastName = "Wayne";
+            user.Age = 30;
+
+            return View(user);
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
+            throw new Exception("Invalid page");
+
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Contact(int id = 0)
         {
-            ViewBag.Message = "Hello!";
+            ViewBag.Message = id;
 
             return View();
         }
